b712c30f6bc2b0352a7c842724a7e3a9cebf60eb
Raymond Natividad <natividad2315@gmail.com>
2022-08-02 00:56:12 -0700

    Learned the switch statement. Learned the difference between statements and expressions. Learned the conditional (ternary) operator. Completed coding challenge #4 with a little bit of help.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -252,29 +252,29 @@ if (favorite !== 23) console.log ('Why not 23?');
 
 
 const hasDriversLicense = true; // A
-const hasGoddVision = true; // B
+const hasGoodVision = true; // B
 
-console.log(hasDriversLicense && hasGoddVision);
-console.log(hasDriversLicense || hasGoddVision);
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
 
-const shouldDrive = hasDriversLicense && hasGoddVision;
+const shouldDrive = hasDriversLicense && hasGoodVision;
 
-// if(hasDriversLicense && hasGoddVision) {
+// if(hasDriversLicense && hasGoodVision) {
 //       console.log('Sarah is able to drive!');
 // } else {
 //       console.log('Someone else should drive...');
 // }
 
 const isTired = false; // C
-console.log(hasDriversLicense && hasGoddVision && isTired);
+console.log(hasDriversLicense && hasGoodVision && isTired);
 
-if(hasDriversLicense && hasGoddVision && !isTired) {
+if(hasDriversLicense && hasGoodVision && !isTired) {
       console.log('Sarah is able to drive!');
 } else {
       console.log('Someone else should drive...');
 }
-*/
+
 
 // Coding Challenge #3
 
@@ -305,3 +305,81 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
       console.log('No one wins trophy 😭');
 }
+
+
+const day = 'friday';
+
+switch (day) {
+      case 'monday': // day === 'monday'
+      console.log ('Plan course structure');
+      console.log ('Go to coding meetup');
+      break;
+      case 'tuesday':
+      console.log ('Prepare theory videos');
+      break;
+      case 'wednesday':
+      case 'thursday':
+      console.log ('Write code examples');
+      break;
+      case 'friday':
+      console.log ('Record videos');
+      break;
+      case 'saturday':
+      case 'sunday':
+      console.log ('Enjoy the weekend :D');
+      break;
+      default:
+      console.log ('Not a valid day!');
+}
+
+if (day === 'monday') {
+      console.log ('Plan course structure');
+      console.log ('Go to coding meetup');
+} else if (day === 'tuesday') {
+      console.log ('Prepare theory videos');
+} else if (day ==='wednesday' || day === 'thursday') {
+      console.log ('Write code examples');
+} else if (day === 'friday') {
+      console.log ('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+      console.log ('Enjoy the weekend :D');
+} else {
+      console.log ('Not a valid day!');
+}
+
+
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+      const str = '23 is bigger';
+}
+
+const me = 'Jonas'
+console.log (`I'm ${2037 - 1991} years old ${me}`);
+
+
+const age = 23;
+// age >= 18 ? console.log ('I like to drink wine 🍷') : console.log ('I like to drink water 💧'); 
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log (drink);
+
+let drink2;
+if (age >= 18) {
+      drink2 = 'wine 🍷';
+} else {
+      drink2 = 'water 💧';
+}
+console.log (drink2);
+
+console.log (`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
+*/
+
+
+// Coding Challenge #4
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+console.log (`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
