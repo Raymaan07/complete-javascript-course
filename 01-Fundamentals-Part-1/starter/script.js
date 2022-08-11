cf9f9e9ab9381579633a063b7b080e452111da88
Raymond Natividad <natividad2315@gmail.com>
2022-08-10 17:55:14 -0700

    Compared my section 1 code to instructor's section 1 code. Got a brief history lesson of JavaScript. Learned about activating 'Strict mode.' Learned about functions.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,11 @@
 /*
+////////////////////////////////////
+// Linking JavaScript File
 let js = 'amazing';
-      console.log( 40 + 8 + 23 - 10);
+console.log( 40 + 8 + 23 - 10);
 
+////////////////////////////////////
+// Values and Variables
 console.log('Jonas');
 console.log(23);
 
@@ -12,20 +16,22 @@ console.log(firstName);
 console.log(firstName);
 
 // Variable name conventions
-let jonas_matilda = 'JM' 
+let jonas_matilda = 'JM';
 let $function = 27;
 
-let person = 'jonas'
+let person = "jonas";
 let PI = 3.1415;
 
-let myFirstJob = 'Coder';
-let myCurrentJob = 'Teacher';
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+let job1 = "programmer";
+let job2 = "teacher";
 
 console.log(myFirstJob);
 
+////////////////////////////////////
+// Data Types
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -44,9 +50,10 @@ console.log(typeof year);
 year = 1991; 
 console.log(typeof year);
 
-console.log (typeof null); 
+console.log(typeof null); 
 
-
+////////////////////////////////////
+// let, const and var
 let age = 30; 
 age = 31;
 
@@ -55,18 +62,20 @@ const birthYear = 1991;
 // const job; 
 
 var job = 'programmer';
-job = 'teacher';
+job = 'teacher'
 
 lastName = 'Schmedtmann';
 console.log(lastName);
 
+////////////////////////////////////
+// Basic Operators
 // Math operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2** 3)
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
 const firstName = 'Jonas';
@@ -76,7 +85,7 @@ console.log(firstName + ' ' + lastName);
 // Assignment operators
 let x = 10 + 5; // 15
 x += 10; // x = x + 10 = 25
-x *=4 // x = x * 4 = 100
+x *= 4; // x = x * 4 = 100
 x++; // x = x + 1
 x--;
 x--;
@@ -90,6 +99,8 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
 
+////////////////////////////////////
+// Operator Precedence
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -97,20 +108,35 @@ const ageSarah = now - 2018;
 console.log(now - 1991 > now - 2018);
 
 let x, y;
-x = y = 25 - 10 - 5; // x = y = 10
-console.log(x,y);
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) / 2
+const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+*/
 
+////////////////////////////////////
+// Coding Challenge #1 
 
-// Coding challenge #1 
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
 
 // const massMark = 78;
 // const heightMark = 1.69;
 // const massJohn = 92;
 // const heightJohn = 1.95;
 
+/*
 const massMark = 95;
 const heightMark = 1.88;
 const massJohn = 85;
@@ -122,15 +148,17 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
 
+////////////////////////////////////
+// Strings and Template Literals
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
 const year = 2037;
 
-const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
-console.log (jonas);
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
 
-const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
 
 console.log(`Just a regular string...`);
@@ -144,10 +172,12 @@ multiple
 lines`);
 
 
+////////////////////////////////////
+// Taking Decisions: if / else Statements
 const age = 15;
 
-if ( age >= 18) {
-console.log(`Sarah can start driving license 🚗`);
+if (age >= 18) {
+  console.log(`Sarah can start driving license 🚗`);
 } else {
   const yearsLeft = 18 - age;
   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);    
@@ -156,8 +186,8 @@ console.log(`Sarah can start driving license 🚗`);
 const birthYear = 2012;
 
 let century;
-if (birthYear<= 2000){
-      century = 20;
+if (birthYear <= 2000) {
+  century = 20;
 } else {
       century = 21;
 }
@@ -383,3 +413,4 @@ console.log (`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
 const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
 console.log (`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
+
